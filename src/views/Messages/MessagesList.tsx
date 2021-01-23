5b1b38f24b9abb79d8fec26a16fd5542faa04f66
Amanda <amanda.chen@connect.qut.edu.au>
2021-01-23 16:17:40 +1000

    Handle no messages

--- src/views/Messages/MessagesList.tsx
+++ src/views/Messages/MessagesList.tsx
@@ -22,45 +22,49 @@ interface IMessagesListProps {
 export const MessagesList = (props: IMessagesListProps) => {
   return (
     <Box className={styles.list}>
-      {props.messages.map((m, i) => (
-        <List key={i} className={styles.message}>
-          <ListItem
-            button
-            onClick={() =>
-              props.setSelectedMessage && props.setSelectedMessage(m)
-            }
-            alignItems="flex-start"
-            classes={{ selected: styles.messageSelected }}
-            className={styles.messageListContainer}
-            selected={props.selectedMessage === m}
-          >
-            <ListItemAvatar>
-              <Avatar classes={{ root: styles.avatar }}>
-                {m.messageId.slice(0, 1)}
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary={
-                <div className={styles.messageTitle}>
-                  {m.from}
-                  <span>{moment(m.sentTimestamp).format("DD/MM/YYYY")}</span>
-                </div>
+      {props.messages.length > 0 ? (
+        props.messages.map((m, i) => (
+          <List key={i} className={styles.message}>
+            <ListItem
+              button
+              onClick={() =>
+                props.setSelectedMessage && props.setSelectedMessage(m)
               }
-              secondary={
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={styles.inline}
-                  color="textSecondary"
-                >
-                  {m.body}
-                </Typography>
-              }
-            />
-          </ListItem>
-          <Divider variant="middle" component="li" />
-        </List>
-      ))}
+              alignItems="flex-start"
+              classes={{ selected: styles.messageSelected }}
+              className={styles.messageListContainer}
+              selected={props.selectedMessage === m}
+            >
+              <ListItemAvatar>
+                <Avatar classes={{ root: styles.avatar }}>
+                  {m.messageId.slice(0, 1)}
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <div className={styles.messageTitle}>
+                    {m.from}
+                    <span>{moment(m.sentTimestamp).format("DD/MM/YYYY")}</span>
+                  </div>
+                }
+                secondary={
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={styles.inline}
+                    color="textSecondary"
+                  >
+                    {m.body}
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <Divider variant="middle" component="li" />
+          </List>
+        ))
+      ) : (
+        <>You have no messages</>
+      )}
     </Box>
   );
 };
